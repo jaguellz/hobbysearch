24bda5f3ba7ab8e804ecd30e17027c8ba18ec327
jaguellz <jaguellz@outlook.com>
2019-04-25 18:36:51 +0400

    idk2

--- head.php
+++ head.php
@@ -1,13 +1,12 @@
 <?php
 $head="<div class='main-url'>
 <a href='index.php' style='margin-right: 15px;'>Главная</a>
-<ul class='topmenu'>
-<li style='margin-right: 15px;'>
-    <div style='display:flex; flex-direction:row; float:right;'>Контакты</div>
-        <div class=submenu style='padding-bottom: 7px; padding-left:5px; padding-top:5px;'>
-        Алишер:+7(999)647-04-27 <br>
-        Роман:+7(999)724-52-37 <br></div>
-</li>
-</ul>
 <a href='about.php'>О нас</a>
 </div>";
+$footer="контакты:<br>
+<a href='https://vk.com/qzynoc'><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/VK.com-logo.svg/768px-VK.com-logo.svg.png' width='32px' height='32px'></a>
+<a href='https://www.instagram.com/qzynoc/'><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/1024px-Instagram_logo_2016.svg.png' width='32px' height='32px'></a>
++7(999)647-04-27 <br>
+<a href='https://vk.com/id242226084'><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/VK.com-logo.svg/768px-VK.com-logo.svg.png' width='32px' height='32px'></a>
+<a href='https://www.instagram.com/noth.ink'><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/1024px-Instagram_logo_2016.svg.png' width='32px' height='32px'></a>        
++7(999)724-52-37 <br>";
